10d0b5b1fdbc1c76102afb30993c2b62b58c6f1c
matteopelusi7 <pelusimatteo02@gmail.com>
2022-12-01 12:51:43 +0100

    add cart and cartitem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,29 @@
 import React from "react";
+import {MdDelete} from 'react-icons/md'
+import {BiPlus, BiMinus} from 'react-icons/bi'
 
-const CartItem = () => {
+const CartItem = ({_id, name, image, price, countInStock}) => {
   return (
     <article className="cart-item">
-        CartItem
+      <div className="img-container">
+        <img src={image} alt={name} className='img' />
+      </div>
+      <p className="prd-name">{name}</p>
+      <div className="qty-selector">
+        <button className="btn icon-btn">
+          <BiPlus className="icon" />
+        </button>
+        <p>
+          1
+        </p>
+        <button className="btn icon-btn">
+          <BiMinus className="icon minus-icon" />
+        </button>
+      </div>
+      <p>{price} &euro;</p>
+      <button className="btn icon-btn">
+        <MdDelete className="icon minus-icon" />
+      </button>
     </article>
   );
 };
